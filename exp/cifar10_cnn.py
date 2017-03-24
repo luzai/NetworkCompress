a7a4e9be0fac35069171aa2c44b0a4da20441423
Wang Xinglu <907682447@qq.com>
2017-03-24 15:25:01 +0800

    move cifar10_cnn to exp

---
+++ exp/cifar10_cnn.py
@@ -0,0 +1,148 @@
+'''Train a simple deep CNN on the CIFAR10 small images dataset.
+
+GPU run command with Theano backend (with TensorFlow, the GPU is automatically used):
+    THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python cifar10_cnn.py
+
+It gets down to 0.65 test logloss in 25 epochs, and down to 0.55 after 50 epochs.
+(it's still underfitting at that point, though).
+'''
+import sys
+sys.path.insert(0,"../src")
+
+from net2net import *
+
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+
+batch_size = 256
+nb_classes = 10
+nb_epoch = 50000 # 3
+data_augmentation = True
+
+# input image dimensions
+img_rows, img_cols = 32, 32
+# The CIFAR10 images are RGB.
+img_channels = 3
+
+# The data, shuffled and split between train and test sets:
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# Convert class vectors to binary class matrices.
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+def limit_data(x):
+    return x[:x.shape[0] / 5]
+
+X_train, Y_train, X_test, Y_test=map(limit_data,[X_train, Y_train, X_test, Y_test])
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+from keras import backend as K
+from keras.layers import Dense
+
+def reset_weights(model):
+    session = K.get_session()
+    for layer in model.layers:
+        if isinstance(layer, Dense):
+            old = layer.get_weights()
+            layer.W.initializer.run(session=session)
+            layer.b.initializer.run(session=session)
+            print(np.array_equal(old, layer.get_weights())," after initializer run")
+        else:
+            print(layer, "not reinitialized")
+
+sav=[]
+for use_dropout in [True,False]:
+    for data_augmentation in [True,False]:
+        if 'model' in locals():
+            del model
+        model = Sequential()
+
+        model.add(Convolution2D(32, 3, 3, border_mode='same',
+                                input_shape=X_train.shape[1:]))
+        model.add(Activation('relu'))
+        model.add(Convolution2D(32, 3, 3))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        if use_dropout:
+            model.add(Dropout(0.25))
+
+        model.add(Convolution2D(64, 3, 3, border_mode='same'))
+        model.add(Activation('relu'))
+        model.add(Convolution2D(64, 3, 3))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        if use_dropout:
+            model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(512))
+        model.add(Activation('relu'))
+        if use_dropout:
+            model.add(Dropout(0.5))
+        model.add(Dense(nb_classes))
+        model.add(Activation('softmax'))
+
+        # Let's train the model using RMSprop
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='rmsprop',
+                      metrics=['accuracy'])
+        # print(model.get_weights())
+        model.summary()
+        model=shuffle_weights(model)
+
+
+        if not data_augmentation:
+            print('Not using data augmentation.')
+            history=model.fit(X_train, Y_train,
+                      batch_size=batch_size,
+                      nb_epoch=nb_epoch,
+                      validation_data=(X_test, Y_test),
+                      shuffle=True,verbose=2)
+        else:
+            print('Using real-time data augmentation.')
+            # This will do preprocessing and realtime data augmentation:
+            datagen = ImageDataGenerator(
+                featurewise_center=False,  # set input mean to 0 over the dataset
+                samplewise_center=False,  # set each sample mean to 0
+                featurewise_std_normalization=False,  # divide inputs by std of the dataset
+                samplewise_std_normalization=False,  # divide each input by its std
+                zca_whitening=False,  # apply ZCA whitening
+                rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+                width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+                height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+                horizontal_flip=True,  # randomly flip images
+                vertical_flip=False)  # randomly flip images
+
+            # Compute quantities required for featurewise normalization
+            # (std, mean, and principal components if ZCA whitening is applied).
+            datagen.fit(X_train)
+
+            # Fit the model on the batches generated by datagen.flow().
+            history=model.fit_generator(datagen.flow(X_train, Y_train,
+                                             batch_size=batch_size),
+                                samples_per_epoch=X_train.shape[0],
+                                nb_epoch=nb_epoch,
+                                validation_data=(X_test, Y_test),
+                                        verbose=2)
+        print([0]+history.history['val_acc'])
+        sav+=[[0]+history.history['val_acc']]
+
+pprint(sav)
+np.save("acc.npy",sav)
+for acc in sav:
+    plt.plot(acc)
+
+plt.legend(["dropout+agu","dp","agu","neither"])
+plt.show()
+plt.savefig("acc.png")
