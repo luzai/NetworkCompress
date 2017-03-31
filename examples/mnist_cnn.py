22ed9a5ebbf2c4091f4d194f9c9461c49a502157
L uzai <L uzai>
2017-03-31 02:32:00 +0100

    More exp on the effect of dropout& to net2wider

--- examples/mnist_cnn.py
+++ examples/mnist_cnn.py
@@ -31,11 +31,11 @@ kernel_size = (3, 3)
 
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-
+print(X_train.shape)
 if K.image_dim_ordering() == 'th':
     X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
     X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
+    input_shape = ( 1,img_rows, img_cols)
 else:
     X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
     X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
--- src/test.py
+++ src/test.py
@@ -4,96 +4,48 @@ sys.path.insert(0,"/home/xlwang/NetworkCompress/src")
 from net2net import *
 input_shape=(64,16,16)
 
-def make_model_for_conv_fc(width):
+def make_model_for_conv_fc(width,with_activation=False,with_max_pool=False,with_dropout=False):
     input_node=Input(shape=(input_shape),name="input")
     x = Convolution2D(64, 3, 3, name='conv7'
-                      # ,activation='relu'
+                      ,activation='relu' if  with_activation else None
                       )(input_node)
     x=Convolution2D(width,3,3,name='conv8'
-                    # ,activation='relu'
+                    ,activation='relu'if  with_activation else None
                     )(x)
-    # x=MaxPooling2D(name='pool8')(x)
-    # x=Dropout(0.25)(x)
+    x=MaxPooling2D(name="pool1")(x)  if with_max_pool else x
+    x=Dropout(0.25,name="drop1")(x) if with_dropout else x
     x=Flatten(name="flatten")(x)
     output_node=Dense(64,name='fc1'
-            # ,activation='relu'
+            ,activation='relu'if  with_activation else None
             )(x)
     model=Model(input=input_node,output=output_node)
     model.trainable=False
     return model
 
-def make_model_for_conv_conv(width):
+def make_model_for_conv_conv(width,with_activation=False,with_max_pool=False,with_dropout=False):
     input_node=Input(shape=(input_shape),name="input")
     x = Convolution2D(width, 3, 3, name='conv7'
-                      # ,activation='relu'
+                      , activation='relu' if with_activation else None
                       )(input_node)
     x=Convolution2D(256,3,3,name='conv8'
-                    # ,activation='relu'
+                    , activation='relu' if with_activation else None
                     )(x)
-    # x=MaxPooling2D(name='pool8')(x)
-    # x=Dropout(0.25)(x)
+    x = MaxPooling2D(name="pool1")(x) if with_max_pool else x
+    x = Dropout(0.25,name="drop1")(x) if with_dropout else x
     x=Flatten(name="flatten")(x)
     output_node=Dense(64,name='fc1'
-            # ,activation='relu'
+                      , activation='relu' if with_activation else None
             )(x)
     model=Model(input=input_node,output=output_node)
     model.trainable=False
     return model
 
-
 np.random.seed(1)
-model=make_model_for_conv_conv(64)
-input_inst=np.random.random((1,)+input_shape)
-output_inst=K.function(inputs=[model.input, K.learning_phase()], outputs=[model.output])([input_inst,0])
-# plt.switch_backend("TkAgg")
-# get_ipython().magic(u'matplotlib inline')
-print output_inst[0].ravel()[:5]
-plt.figure()
-plt.imshow(input_inst[0][0])
-plt.figure()
-plt.imshow(output_inst[0])
-_=plt.axis("off")
 
-
-model2=make_model_for_conv_conv(256)
-print [l.name for l in model.layers]
-print [l.name for l in model2.layers]
-
-
-copy_weights(student_model=model2,teacher_model=model)
-w_conv1, b_conv1 = model.get_layer("conv7").get_weights()
-w_conv2, b_conv2 = model.get_layer("conv8").get_weights()
-new_w_conv1, new_b_conv1, new_w_conv2 = wider_conv2d_weight(
-    w_conv1, b_conv1, w_conv2, 256, "net2wider")
-model2.get_layer("conv7").set_weights([new_w_conv1, new_b_conv1])
-model2.get_layer("conv8").set_weights([new_w_conv2, b_conv2])
-
-output_inst2=K.function(inputs=[model2.input, K.learning_phase()], outputs=[model2.output])([input_inst,0])
-# plt.switch_backend("TkAgg")
-# get_ipython().magic(u'matplotlib inline')
-print output_inst2[0].ravel()[:5]
-plt.figure()
-plt.imshow(output_inst2[0])
-_=plt.axis("off")
-
-print output_inst[0].sum(),output_inst[0].sum()
-
-np.random.seed(1)
 model=make_model_for_conv_fc(64)
+model2=make_model_for_conv_fc(256)
 input_inst=np.random.random((1,)+input_shape)
 output_inst=K.function(inputs=[model.input, K.learning_phase()], outputs=[model.output])([input_inst,0])
-# plt.switch_backend("TkAgg")
-# get_ipython().magic(u'matplotlib inline')
-print output_inst[0].ravel()[:5]
-plt.figure()
-plt.imshow(input_inst[0][0])
-plt.figure()
-plt.imshow(output_inst[0])
-_=plt.axis("off")
-
-model2=make_model_for_conv_fc(256)
-print [l.name for l in model.layers]
-print [l.name for l in model2.layers]
 
 copy_weights(student_model=model2,teacher_model=model)
 w_conv1, b_conv1 = model.get_layer("conv8").get_weights()
@@ -103,14 +55,8 @@ new_w_conv1, new_b_conv1 = wider_conv2d_weight(
 model2.get_layer("conv8").set_weights([new_w_conv1, new_b_conv1])
 
 output_inst2=K.function(inputs=[model2.input, K.learning_phase()], outputs=[model2.output])([input_inst,0])
-# plt.switch_backend("TkAgg")
-# get_ipython().magic(u'matplotlib inline')
-print output_inst2[0].ravel()[:5]
-plt.figure()
-plt.imshow(output_inst2[0])
-_=plt.axis("off")
-
-print output_inst[0].sum(),output_inst[0].sum()
+print output_inst[0][0][:5]
+print output_inst2[0][0][:5]
 plt.figure()
 plt.plot(output_inst[0][0])
 plt.plot(output_inst2[0][0])
